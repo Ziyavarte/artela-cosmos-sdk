169ee86c3132cd7350e6d37734bb3d3a17cee491
luketheart <eric@artela.network>
2024-05-15 15:30:14 +0800

    fix: add debug log for commit time cost

--- store/rootmulti/store.go
+++ store/rootmulti/store.go
@@ -625,14 +625,10 @@ func (rs *Store) PruneStores(clearPruningManager bool, pruningHeights []int64) (
 		rs.logger.Debug("no heights need to be pruned")
 		return nil
 	}
-	t := time.Now()
-	defer func() {
-		fmt.Printf("_________PruneStores, from: %d, to: %d, duration: %.2fms\n",
-			pruningHeights[0], pruningHeights[len(pruningHeights)-1], float64(time.Since(t).Microseconds())/1000)
-	}()
 
 	rs.logger.Debug("pruning store", "heights", pruningHeights)
 
+	startPruneTime := time.Now()
 	var wg sync.WaitGroup
 	errCh := make(chan error)
 	for key, store := range rs.stores {
@@ -648,13 +644,12 @@ func (rs *Store) PruneStores(clearPruningManager bool, pruningHeights []int64) (
 
 		wg.Add(1)
 		go func(key types.StoreKey, store types.CommitStore) {
-			t1 := time.Now()
-			fmt.Printf("______________DeleteVersions, store: %s\n",
-				key.Name())
+			rs.logger.Debug("DeleteVersions", "store", key.Name())
+			startDeleteTime := time.Now()
 			err := store.(*iavl.Store).DeleteVersions(pruningHeights...)
 			if err == nil {
-				fmt.Printf("______________DeleteVersions, store: %s, duration: %.2fms\n",
-					key.Name(), float64(time.Since(t1).Microseconds())/1000)
+				rs.logger.Debug("DeleteVersions", "store", key.Name(), "cost of time",
+					fmt.Sprintf("%.2fms", float64(time.Since(startDeleteTime).Microseconds())/1000))
 				wg.Done()
 				return
 			}
@@ -675,6 +670,12 @@ func (rs *Store) PruneStores(clearPruningManager bool, pruningHeights []int64) (
 
 	select {
 	case <-done:
+		rs.logger.Debug(
+			"PruneStores",
+			"from", pruningHeights[0],
+			"to", pruningHeights[len(pruningHeights)-1],
+			"cost of time", fmt.Sprintf("%.2fms", float64(time.Since(startPruneTime).Microseconds())/1000),
+		)
 		return nil
 	case err := <-errCh:
 		return err
